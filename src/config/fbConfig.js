1db8d2ae35684ce4d83c5d777bdc8d40b4c117aa
jporter <james.porter@atombank.co.uk>
2020-01-03 15:58:54 +0000

    Changed Firebase config and updated dependencies

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,17 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-// Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyDo2HauU6PtL3PY6KthXdpIUzbDV908avo",
-  authDomain: "marioplan-files.firebaseapp.com",
-  databaseURL: "https://marioplan-files.firebaseio.com",
-  projectId: "marioplan-files",
-  storageBucket: "marioplan-files.appspot.com",
-  messagingSenderId: "1011174152626"
+  apiKey: "AIzaSyCC-VbpyHRP5pu4xy5iRLM5viuLhlewtJA",
+  authDomain: "social-network-dev-845aa.firebaseapp.com",
+  databaseURL: "https://social-network-dev-845aa.firebaseio.com",
+  projectId: "social-network-dev-845aa",
+  storageBucket: "social-network-dev-845aa.appspot.com",
+  messagingSenderId: "674464808555",
+  appId: "1:674464808555:web:0e706e6e2a2e048929bb2e",
+  measurementId: "G-M9B84ETVS5"
 };
 firebase.initializeApp(config);
-firebase.firestore().settings({ timestampsInSnapshots: true });
+firebase.firestore().settings({});
 
-export default firebase 
+export default firebase;
